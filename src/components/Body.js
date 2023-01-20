6b3b60de969d57b11abd7b60f8553557c066188a
sushant3902 <sushant.subhangam@gmail.com>
2023-01-20 23:24:23 +0530

    Updates

---
+++ src/components/Body.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { restaurantList } from "../constants";
+import RestaurantCard from "./RestaurantCard";
+
+function filterRestaurant(searchText, restaurants) {
+    const filterData = restaurants.filter((res) => {
+        return res.data.data.name.includes(searchText)
+    });
+
+    return filterData;
+}
+
+const Body = () => {
+    // let searchTxt = "KFC";
+    const [searchInput, setSearchInput] = useState("")
+    const [restaurants, setReataurants] = useState(restaurantList)
+    return (
+        <>
+            <div className="search-container">
+                <input type="text" className="search-input" placeholder="" value={searchInput } onChange={(e) => {
+                    setSearchInput(e.target.value)
+                }} />
+                <button className="search-btn" onClick={() => {
+                    const data = filterRestaurant(searchInput, restaurants);
+                    setReataurants(data)
+                }}>Search</button>
+            </div>
+            <div className="restaurant-list">
+            {restaurants.map((restaurant) => {
+                return <RestaurantCard {...restaurant.data.data} key={restaurant.data.data.id}/>
+            })}
+            </div>
+        </>
+    );
+};
+
+export default Body;
